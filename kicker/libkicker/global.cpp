841194d1bffdf87bcdbae758777e6c88aef6bf1d
tpearson <tpearson@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2011-02-23 03:39:57 +0000

    Fix kicker minipager hover shading issue under Qt3
    git-svn-id: svn://anonsvn.kde.org/home/kde/branches/trinity/kdebase@1222358 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- kicker/libkicker/global.cpp
+++ kicker/libkicker/global.cpp
@@ -470,8 +470,14 @@ void drawBlendedRect(TQPainter *p, const TQRect &r, const TQColor &color, int al
     if (pix.isNull() || last_color != color || last_alpha != alpha)
     {
         TQImage img(16, 16, 32);
+#ifdef USE_QT4
         img.setAlphaBuffer(true);
-        img.fill(((uint)(alpha & 0xFF) << 24) | (color.rgb() & 0xFFFFFF));
+	img.fill(((uint)(alpha & 0xFF) << 24) | (color.rgb() & 0xFFFFFF));
+#else // USE_QT4
+	img.setAlphaBuffer(false);
+	img.fill(((uint)(alpha & 0xFF) << 24) | (color.rgb() & 0xFFFFFF));
+	img.setAlphaBuffer(true);
+#endif // USE_QT4
         pix.convertFromImage(img);
         last_color = color;
         last_alpha = alpha;
