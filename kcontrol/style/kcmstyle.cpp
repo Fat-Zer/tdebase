Fat-Zer/tdebase

commit cfd4a7e4ea2276df469207aa5041cec412861493

Branding cleanup: KDE -> TDE

kcontrol/style/kcmstyle.cpp
@@ -73,21 +73,21 @@
 // Danimo: Why do we use the old interface?!
 extern "C"
 {
-    KDE_EXPORT KCModule *create_style(TQWidget *parent, const char*)
+    _EXPORT KCModule *create_style(TQWidget *parent, const char*)
     {
         KGlobal::locale()->insertCatalogue("kcmstyle");
         return new KCMStyle(parent, "kcmstyle");
     }
 
-    KDE_EXPORT void init_style()
+    _EXPORT void init_style()
     {
         uint flags = KRdbExportQtSettings | KRdbExportQtColors | KRdbExportXftSettings;
         KConfig config("kcmdisplayrc", true /*readonly*/, false /*don't read kdeglobals etc.*/);
         config.setGroup("X11");
 
         // This key is written by the "colors" module.
-        bool exportKDEColors = config.readBoolEntry("exportKDEColors", true);
-        if (exportKDEColors)
+        bool exportColors = config.readBoolEntry("exportColors", true);
+        if (exportColors)
             flags |= KRdbExportColors;
         runRdb( flags );
 
@@ -469,7 +469,7 @@ void KCMStyle::styleSpecificConfig()
 		// Force re-rendering of the preview, to apply settings
 		switchStyle(currentStyle(), true);
 
-		//For now, ask all KDE apps to recreate their styles to apply the setitngs
+		//For now, ask all  apps to recreate their styles to apply the setitngs
 		KIPC::sendMessageAll(KIPC::StyleChanged);
 
 		// We call setStyleDirty here to make sure we force style re-creation
@@ -560,7 +560,7 @@ void KCMStyle::save()
 
 	// Save effects.
 	KConfig config( "kdeglobals" );
-	config.setGroup("KDE");
+	config.setGroup("");
 
 	config.writeEntry( "EffectsEnabled", cbEnableEffects->isChecked());
 	int item = comboComboEffect->currentItem();
@@ -632,13 +632,13 @@ void KCMStyle::save()
 		uint flags = KRdbExportQtSettings;
 		KConfig kconfig("kcmdisplayrc", true /*readonly*/, false /*no globals*/);
 		kconfig.setGroup("X11");
-		bool exportKDEColors = kconfig.readBoolEntry("exportKDEColors", true);
-		if (exportKDEColors)
+		bool exportColors = kconfig.readBoolEntry("exportColors", true);
+		if (exportColors)
 			flags |= KRdbExportColors;
 		runRdb( flags );
 	}
 
-	// Now allow KDE apps to reconfigure themselves.
+	// Now allow  apps to reconfigure themselves.
 	if ( m_bStyleDirty )
 		KIPC::sendMessageAll(KIPC::StyleChanged);
 
@@ -727,10 +727,10 @@ void KCMStyle::loadStyle( KConfig& config )
 	for (TQStringList::iterator it = list.begin(); it != list.end(); ++it)
 	{
 		KSimpleConfig config( *it, true );
-		if ( !(config.hasGroup("KDE") && config.hasGroup("Misc")) )
+		if ( !(config.hasGroup("") && config.hasGroup("Misc")) )
 			continue;
 
-		config.setGroup("KDE");
+		config.setGroup("");
 
 		strWidgetStyle = config.readEntry("WidgetStyle");
 		if (strWidgetStyle.isNull())
@@ -893,7 +893,7 @@ void KCMStyle::setStyleRecursive(TQWidget* w, TQStyle* s)
 void KCMStyle::loadEffects( KConfig& config )
 {
 	// Load effects.
-	config.setGroup("KDE");
+	config.setGroup("");
 
 	cbEnableEffects->setChecked( config.readBoolEntry( "EffectsEnabled", false) );
 
@@ -1009,7 +1009,7 @@ void KCMStyle::menuEffectChanged( bool enabled )
 
 void KCMStyle::loadMisc( KConfig& config )
 {
-	// KDE's Part via KConfig
+	// 's Part via KConfig
 	config.setGroup("Toolbar style");
 	cbHoverButtons->setChecked(config.readBoolEntry("Highlighting", true));
 	cbTransparentToolbars->setChecked(config.readBoolEntry("TransparentMoving", true));
@@ -1024,7 +1024,7 @@ void KCMStyle::loadMisc( KConfig& config )
 	else
 		comboToolbarIcons->setCurrentItem(0);
 
-	config.setGroup("KDE");
+	config.setGroup("");
 	cbIconsOnButtons->setChecked(config.readBoolEntry("ShowIconsOnPushButtons", false));
 	cbEnableTooltips->setChecked(!config.readBoolEntry("EffectNoTooltip", false));
 	cbTearOffHandles->setChecked(config.readBoolEntry("InsertTearOffHandle", false));
@@ -1060,9 +1060,9 @@ void KCMStyle::addWhatsThis()
 	TQWhatsThis::add( comboMenuEffect, i18n( "<p><b>Disable: </b>do not use any menu effects.</p>\n"
 							"<p><b>Animate: </b>Do some animation.</p>\n"
 							"<p><b>Fade: </b>Fade in menus using alpha-blending.</p>\n"
-							"<b>Make Translucent: </b>Alpha-blend menus for a see-through effect. (KDE styles only)") );
+							"<b>Make Translucent: </b>Alpha-blend menus for a see-through effect. ( styles only)") );
 	TQWhatsThis::add( cbMenuShadow, i18n( "When enabled, all popup menus will have a drop-shadow, otherwise "
-							"drop-shadows will not be displayed. At present, only KDE styles can have this "
+							"drop-shadows will not be displayed. At present, only  styles can have this "
 							"effect enabled.") );
 	TQWhatsThis::add( comboMenuEffectType, i18n( "<p><b>Software Tint: </b>Alpha-blend using a flat color.</p>\n"
 							"<p><b>Software Blend: </b>Alpha-blend using an image.</p>\n"
@@ -1078,7 +1078,7 @@ void KCMStyle::addWhatsThis()
 							"their color when the mouse cursor is moved over them." ) );
 	TQWhatsThis::add( cbTransparentToolbars, i18n("If you check this box, the toolbars will be "
 							"transparent when moving them around.") );
-	TQWhatsThis::add( cbEnableTooltips, i18n( "If you check this option, the KDE application "
+	TQWhatsThis::add( cbEnableTooltips, i18n( "If you check this option, the TDE application "
 							"will offer tooltips when the cursor remains over items in the toolbar." ) );
 	TQWhatsThis::add( comboToolbarIcons, i18n( "<p><b>Icons only:</b> Shows only icons on toolbar buttons. "
 							"Best option for low resolutions.</p>"

kate/app/kwritemain.cpp
@@ -79,8 +79,8 @@ KWrite::KWrite (KTextEditor::Document *doc)
   {
     if ( !(doc = KTextEditor::EditorChooser::createDocument(0,"KTextEditor::Document")) )
     {
-      KMessageBox::error(this, i18n("A KDE text-editor component could not be found;\n"
-                                    "please check your KDE installation."));
+      KMessageBox::error(this, i18n("A TDE text-editor component could not be found;\n"
+                                    "please check your TDE installation."));
       kapp->exit(1);
     }
 

kcontrol/fonts/fonts.cpp
@@ -624,7 +624,7 @@ KFonts::KFonts(TQWidget *parent, const char *name, const TQStringList &)
    cbAA->insertItem( i18n( "Enabled" )); // change AASetting type if order changes
    cbAA->insertItem( i18n( "System settings" ));
    cbAA->insertItem( i18n( "Disabled" ));
-   TQWhatsThis::add(cbAA, i18n("If this option is selected, KDE will smooth the edges of curves in "
+   TQWhatsThis::add(cbAA, i18n("If this option is selected, TDE will smooth the edges of curves in "
                               "fonts."));
    aaSettingsButton = new TQPushButton( i18n( "Configure..." ), this);
    connect(aaSettingsButton, TQT_SIGNAL(clicked()), TQT_SLOT(slotCfgAa()));

kcontrol/keys/keyconfig.cpp
@@ -131,7 +131,7 @@ void KKeyModule::init( bool isGlobal, bool _bSeriesOnly, bool bSeriesNone )
   removeBt->setEnabled(FALSE);
   connect( removeBt, TQT_SIGNAL( clicked() ), TQT_SLOT( slotRemove() ) );
   TQWhatsThis::add( removeBt, i18n("Click here to remove the selected key bindings scheme. You can not"
-    " remove the standard system wide schemes, 'Current scheme' and 'KDE default'.") );
+    " remove the standard system wide schemes, 'Current scheme' and 'TDE default'.") );
 
   // Hack to get this setting only displayed once.  It belongs in main.cpp instead.
   // That move will take a lot of UI redesigning, though, so i'll do it once CVS
@@ -145,7 +145,7 @@ void KKeyModule::init( bool isGlobal, bool _bSeriesOnly, bool bSeriesNone )
 	preferMetaBt->setChecked( KKeySequence::useFourModifierKeys() );
 	connect( preferMetaBt, TQT_SIGNAL(clicked()), TQT_SLOT(slotPreferMeta()) );
 	TQWhatsThis::add( preferMetaBt, i18n("If your keyboard has a Meta key, but you would "
-		"like KDE to prefer the 3-modifier configuration defaults, then this option "
+		"like TDE to prefer the 3-modifier configuration defaults, then this option "
 		"should be unchecked.") );
   } else
 	preferMetaBt = 0;
@@ -434,18 +434,18 @@ void KKeyModule::readScheme( int index )
   sFileList->clear();
   sList->insertItem( i18n("Current Scheme"), 0 );
   sFileList->append( "Not a kcsrc file" );
-  sList->insertItem( i18n("KDE Traditional"), 1 );
+  sList->insertItem( i18n("TDE Traditional"), 1 );
   sFileList->append( "Not a kcsrc file" );
-  //sList->insertItem( i18n("KDE Extended (With 'Win' Key)"), 2 );
-  //sList->insertItem( i18n("KDE Default for 4 Modifiers (Meta/Alt/Ctrl/Shift)"), 2 );
+  //sList->insertItem( i18n("TDE Extended (With 'Win' Key)"), 2 );
+  //sList->insertItem( i18n("TDE Default for 4 Modifiers (Meta/Alt/Ctrl/Shift)"), 2 );
   //sFileList->append( "Not a kcsrc file" );
   nSysSchemes = 2;
 
   // This for system files
   for ( TQStringList::ConstIterator it = schemes.begin(); it != schemes.end(); ++it) {
     // KPersonalizer relies on .kksrc files containing all the keyboard shortcut
-    //  schemes for various setups.  It also requires the KDE defaults to be in
-    //  a .kksrc file.  The KDE defaults shouldn't be listed here.
+    //  schemes for various setups.  It also requires the TDE defaults to be in
+    //  a .kksrc file.  The TDE defaults shouldn't be listed here.
     //if( r.search( *it ) != -1 )
     //   continue;
 

kcontrol/keys/shortcuts.cpp
@@ -95,9 +95,9 @@ TQString ShortcutsModule::quickHelp() const
 {
   return i18n("<h1>Key Bindings</h1> Using key bindings you can configure certain actions to be"
     " triggered when you press a key or a combination of keys, e.g. Ctrl+C is normally bound to"
-    " 'Copy'. KDE allows you to store more than one 'scheme' of key bindings, so you might want"
+    " 'Copy'. TDE allows you to store more than one 'scheme' of key bindings, so you might want"
     " to experiment a little setting up your own scheme while you can still change back to the"
-    " KDE defaults.<p> In the tab 'Global Shortcuts' you can configure non-application specific"
+    " TDE defaults.<p> In the tab 'Global Shortcuts' you can configure non-application specific"
     " bindings like how to switch desktops or maximize a window. In the tab 'Application Shortcuts'"
     " you will find bindings typically used in applications, such as copy and paste.");
 }
@@ -155,7 +155,7 @@ void ShortcutsModule::initGUI()
 	m_pbtnRemove->setEnabled( false );
 	connect( m_pbtnRemove, TQT_SIGNAL(clicked()), TQT_SLOT(slotRemoveScheme()) );
 	TQWhatsThis::add( m_pbtnRemove, i18n("Click here to remove the selected key bindings scheme. You cannot"
-		" remove the standard system-wide schemes 'Current scheme' and 'KDE default'.") );
+		" remove the standard system-wide schemes 'Current scheme' and 'TDE default'.") );
 	pHLayout->addWidget( m_pbtnRemove );
 
 	pHLayout->addSpacing( KDialog::marginHint() * 3 );
@@ -276,8 +276,8 @@ void ShortcutsModule::readSchemeNames()
 	// This for system files
 	for ( TQStringList::ConstIterator it = schemes.begin(); it != schemes.end(); ++it) {
 	// KPersonalizer relies on .kksrc files containing all the keyboard shortcut
-	//  schemes for various setups.  It also requires the KDE defaults to be in
-	//  a .kksrc file.  The KDE defaults shouldn't be listed here.
+	//  schemes for various setups.  It also requires the TDE defaults to be in
+	//  a .kksrc file.  The TDE defaults shouldn't be listed here.
 		//if( r.search( *it ) != -1 )
 		//   continue;
 

kcontrol/kthememanager/kthememanager.cpp
@@ -48,13 +48,13 @@ kthememanager::kthememanager( TQWidget *parent, const char *name )
 
     KAboutData *about = new KAboutData("kthememanager", I18N_NOOP("TDE Theme Manager"),
                                        "0.4", I18N_NOOP("This control module handles installing, removing and "
-                                                        "creating visual KDE themes."),
+                                                        "creating visual TDE themes."),
                                        KAboutData::License_GPL, "(c) 2003, 2004, 2006 Lukáš Tinkl", 0,
                                        "http://developer.kde.org/~lukas/kthememanager");
     setAboutData( about );
 
     setQuickHelp( i18n("This control module handles installing, removing and "
-                "creating visual KDE themes."));
+                "creating visual TDE themes."));
 
     setButtons( KCModule::Default|KCModule::Apply|KCModule::Help );
 

kcontrol/performance/konqueror.cpp
@@ -66,13 +66,13 @@ Konqueror::Konqueror( TQWidget* parent_P )
     TQWhatsThis::add( sb_preload_count, tmp );
     TQWhatsThis::add( lb_preload_count, tmp );
     TQWhatsThis::add( cb_preload_on_startup,
-        i18n( "If enabled, an instance of Konqueror will be preloaded after the ordinary KDE "
+        i18n( "If enabled, an instance of Konqueror will be preloaded after the ordinary TDE "
               "startup sequence."
               "<p>This will make the first Konqueror window open faster, but "
-              "at the expense of longer KDE startup times (but you will be able to work "
+              "at the expense of longer TDE startup times (but you will be able to work "
               "while it is loading, so you may not even notice that it is taking longer)." ));
     TQWhatsThis::add( cb_always_have_preloaded,
-        i18n( "If enabled, KDE will always try to have one preloaded Konqueror instance ready; "
+        i18n( "If enabled, TDE will always try to have one preloaded Konqueror instance ready; "
               "preloading a new instance in the background whenever there is not one available, "
               "so that windows will always open quickly."
               "<p><b>Warning:</b> In some cases, it is actually possible that this will "

kcontrol/xinerama/kcmxinerama.cpp
@@ -53,7 +53,7 @@ KCMXinerama::KCMXinerama(TQWidget *parent, const char *name)
 	about->addAuthor("George Staikos", 0, "staikos@kde.org");
 	setAboutData( about );
 
-	setQuickHelp( i18n("<h1>Multiple Monitors</h1> This module allows you to configure KDE support"
+	setQuickHelp( i18n("<h1>Multiple Monitors</h1> This module allows you to configure TDE support"
      " for multiple monitors."));
 
 	config = new KConfig("kdeglobals", false, false);
@@ -184,7 +184,7 @@ void KCMXinerama::save() {
 		ksplashrc->sync();
 	}
 
-	KMessageBox::information(this, i18n("Your settings will only affect newly started applications."), i18n("KDE Multiple Monitors"), "nomorexineramaplease");
+	KMessageBox::information(this, i18n("Your settings will only affect newly started applications."), i18n("TDE Multiple Monitors"), "nomorexineramaplease");
 
 	emit changed(false);
 }

commit b8ecc6232b0b4644483b5d00a6cae04d6de2b857

Fix kcontrol/style/kcmstyle.cpp broken in commit cfd4a7e4ea2276df469207aa5041cec412861493

kcontrol/style/kcmstyle.cpp
@@ -73,21 +73,21 @@
 // Danimo: Why do we use the old interface?!
 extern "C"
 {
-    _EXPORT KCModule *create_style(TQWidget *parent, const char*)
+    TDE_EXPORT KCModule *create_style(TQWidget *parent, const char*)
     {
         KGlobal::locale()->insertCatalogue("kcmstyle");
         return new KCMStyle(parent, "kcmstyle");
     }
 
-    _EXPORT void init_style()
+    TDE_EXPORT void init_style()
     {
         uint flags = KRdbExportQtSettings | KRdbExportQtColors | KRdbExportXftSettings;
         KConfig config("kcmdisplayrc", true /*readonly*/, false /*don't read kdeglobals etc.*/);
         config.setGroup("X11");
 
         // This key is written by the "colors" module.
-        bool exportColors = config.readBoolEntry("exportColors", true);
-        if (exportColors)
+        bool exportTDEColors = config.readBoolEntry("exportTDEColors", true);
+        if (exportTDEColors)
             flags |= KRdbExportColors;
         runRdb( flags );
 
@@ -469,7 +469,7 @@ void KCMStyle::styleSpecificConfig()
 		// Force re-rendering of the preview, to apply settings
 		switchStyle(currentStyle(), true);
 
-		//For now, ask all  apps to recreate their styles to apply the setitngs
+		//For now, ask all TDE apps to recreate their styles to apply the setitngs
 		KIPC::sendMessageAll(KIPC::StyleChanged);
 
 		// We call setStyleDirty here to make sure we force style re-creation
@@ -560,7 +560,7 @@ void KCMStyle::save()
 
 	// Save effects.
 	KConfig config( "kdeglobals" );
-	config.setGroup("");
+	config.setGroup("TDE");
 
 	config.writeEntry( "EffectsEnabled", cbEnableEffects->isChecked());
 	int item = comboComboEffect->currentItem();
@@ -632,13 +632,13 @@ void KCMStyle::save()
 		uint flags = KRdbExportQtSettings;
 		KConfig kconfig("kcmdisplayrc", true /*readonly*/, false /*no globals*/);
 		kconfig.setGroup("X11");
-		bool exportColors = kconfig.readBoolEntry("exportColors", true);
-		if (exportColors)
+		bool exportTDEColors = kconfig.readBoolEntry("exportTDEColors", true);
+		if (exportTDEColors)
 			flags |= KRdbExportColors;
 		runRdb( flags );
 	}
 
-	// Now allow  apps to reconfigure themselves.
+	// Now allow TDE apps to reconfigure themselves.
 	if ( m_bStyleDirty )
 		KIPC::sendMessageAll(KIPC::StyleChanged);
 
@@ -727,10 +727,10 @@ void KCMStyle::loadStyle( KConfig& config )
 	for (TQStringList::iterator it = list.begin(); it != list.end(); ++it)
 	{
 		KSimpleConfig config( *it, true );
-		if ( !(config.hasGroup("") && config.hasGroup("Misc")) )
+		if ( !(config.hasGroup("TDE") && config.hasGroup("Misc")) )
 			continue;
 
-		config.setGroup("");
+		config.setGroup("TDE");
 
 		strWidgetStyle = config.readEntry("WidgetStyle");
 		if (strWidgetStyle.isNull())
@@ -893,7 +893,7 @@ void KCMStyle::setStyleRecursive(TQWidget* w, TQStyle* s)
 void KCMStyle::loadEffects( KConfig& config )
 {
 	// Load effects.
-	config.setGroup("");
+	config.setGroup("TDE");
 
 	cbEnableEffects->setChecked( config.readBoolEntry( "EffectsEnabled", false) );
 
@@ -1009,7 +1009,7 @@ void KCMStyle::menuEffectChanged( bool enabled )
 
 void KCMStyle::loadMisc( KConfig& config )
 {
-	// 's Part via KConfig
+	// TDE's Part via KConfig
 	config.setGroup("Toolbar style");
 	cbHoverButtons->setChecked(config.readBoolEntry("Highlighting", true));
 	cbTransparentToolbars->setChecked(config.readBoolEntry("TransparentMoving", true));
@@ -1024,7 +1024,7 @@ void KCMStyle::loadMisc( KConfig& config )
 	else
 		comboToolbarIcons->setCurrentItem(0);
 
-	config.setGroup("");
+	config.setGroup("TDE");
 	cbIconsOnButtons->setChecked(config.readBoolEntry("ShowIconsOnPushButtons", false));
 	cbEnableTooltips->setChecked(!config.readBoolEntry("EffectNoTooltip", false));
 	cbTearOffHandles->setChecked(config.readBoolEntry("InsertTearOffHandle", false));
@@ -1060,9 +1060,9 @@ void KCMStyle::addWhatsThis()
 	TQWhatsThis::add( comboMenuEffect, i18n( "<p><b>Disable: </b>do not use any menu effects.</p>\n"
 							"<p><b>Animate: </b>Do some animation.</p>\n"
 							"<p><b>Fade: </b>Fade in menus using alpha-blending.</p>\n"
-							"<b>Make Translucent: </b>Alpha-blend menus for a see-through effect. ( styles only)") );
+							"<b>Make Translucent: </b>Alpha-blend menus for a see-through effect. (TDE styles only)") );
 	TQWhatsThis::add( cbMenuShadow, i18n( "When enabled, all popup menus will have a drop-shadow, otherwise "
-							"drop-shadows will not be displayed. At present, only  styles can have this "
+							"drop-shadows will not be displayed. At present, only TDE styles can have this "
 							"effect enabled.") );
 	TQWhatsThis::add( comboMenuEffectType, i18n( "<p><b>Software Tint: </b>Alpha-blend using a flat color.</p>\n"
 							"<p><b>Software Blend: </b>Alpha-blend using an image.</p>\n"