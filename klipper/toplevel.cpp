Fat-Zer/tdebase

commit 745063fab8573daad8f3a174809c2da5d98f4fa6

Avoid excessively long Klipper startup time due to oversized tray icon creation when icon widget has not yet been shown

klipper/toplevel.cpp
@@ -198,12 +198,21 @@ KlipperWidget::KlipperWidget( TQWidget *parent, TDEConfig* config )
 
     if ( isApplet() ) {
         m_pixmap = KSystemTray::loadIcon( "klipper" );
+        m_iconOrigWidth = width();
+        m_iconOrigHeight = height();
     }
     else {
-        m_pixmap = KSystemTray::loadSizedIcon( "klipper", width() );
+        if (isShown()) {
+            m_pixmap = KSystemTray::loadSizedIcon( "klipper", width() );
+            m_iconOrigWidth = width();
+            m_iconOrigHeight = height();
+        }
+        else {
+            m_pixmap = KSystemTray::loadIcon( "klipper" );
+            m_iconOrigWidth = m_pixmap.width();
+            m_iconOrigHeight = m_pixmap,height();
+        }
     }
-    m_iconOrigWidth = width();
-    m_iconOrigHeight = height();
     adjustSize();
 
     globalKeys = new TDEGlobalAccel(TQT_TQOBJECT(this));

commit d8bc25b05db5f799797b69d44f9db2e5c38e9e2a

Fixed FTBFS caused by commit 745063f.

klipper/toplevel.cpp
@@ -210,7 +210,7 @@ KlipperWidget::KlipperWidget( TQWidget *parent, TDEConfig* config )
         else {
             m_pixmap = KSystemTray::loadIcon( "klipper" );
             m_iconOrigWidth = m_pixmap.width();
-            m_iconOrigHeight = m_pixmap,height();
+            m_iconOrigHeight = m_pixmap.height();
         }
     }
     adjustSize();