Fat-Zer/tdebase

commit d3ce4fa7931f1a69fb3f6bd759a864db09ebbdc2

Fix FTBFS due to linux specific headers

kdesktop/lock/lockprocess.cc
@@ -79,7 +79,9 @@
 
 #include <kcrash.h>
 
+#ifdef __Linux__
 #include <linux/stat.h>
+#endif
 
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>

tdm/backend/ctrl.c
@@ -42,7 +42,9 @@ from the copyright holder.
 #include <signal.h>
 #include <pwd.h>
 
+#ifdef __Linux__
 #include <linux/vt.h>
+#endif
 #include "getfd.h"
 
 static void

tdm/backend/getfd.c
@@ -4,7 +4,9 @@
 #include <fcntl.h>
 #include <errno.h>
 #include <sys/ioctl.h>
+#ifdef __Linux__
 #include <linux/kd.h>
+#endif
 #include "getfd.h"
 
 /*

commit 68cfc4fec0fed7ef4da9f03da68bd61900654ee3

Fix FTBFS caused by typo in commit d3ce4fa7

kcontrol/tdefontinst/tdefontinst/GetPid.c
@@ -83,7 +83,7 @@
     6. Email me and let me know if it works!
 */
 
-#if defined OS_Linux || defined __Linux__
+#if defined OS_Linux || defined __linux__
 
 #include <dirent.h>
 #include <ctype.h>

kdesktop/lock/lockprocess.cc
@@ -79,7 +79,7 @@
 
 #include <kcrash.h>
 
-#ifdef __Linux__
+#ifdef __linux__
 #include <linux/stat.h>
 #endif
 

tdm/backend/ctrl.c
@@ -42,7 +42,7 @@ from the copyright holder.
 #include <signal.h>
 #include <pwd.h>
 
-#ifdef __Linux__
+#ifdef __linux__
 #include <linux/vt.h>
 #endif
 #include "getfd.h"

tdm/backend/getfd.c
@@ -4,7 +4,7 @@
 #include <fcntl.h>
 #include <errno.h>
 #include <sys/ioctl.h>
-#ifdef __Linux__
+#ifdef __linux__
 #include <linux/kd.h>
 #endif
 #include "getfd.h"