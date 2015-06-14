4e9aeea22462957753950edc3ccad2549009b178
Slávek Banko <slavek.banko@axis.cz>
2015-06-14 21:22:33 +0200

    Fix openpty detection with util.h and libutil.h
    Signed-off-by: Slávek Banko <slavek.banko@axis.cz>

--- tdeioslave/fish/ConfigureChecks.cmake
+++ tdeioslave/fish/ConfigureChecks.cmake
@@ -18,15 +18,27 @@ check_include_file( pty.h HAVE_PTY_H )
 tde_save( CMAKE_REQUIRED_LIBRARIES )
 set( CMAKE_REQUIRED_LIBRARIES util )
 
-check_c_source_runs("
-    #include <pty.h>
-    int main(int argc, char* argv) {
-      int master_fd, slave_fd;
-      int result;
-      result = openpty(&master_fd, &slave_fd, 0, 0, 0);
-      return 0;
-  }"
-  HAVE_OPENPTY
-)
+if( HAVE_PTY_H )
+  set( USE_OPENPTY_H pty.h )
+elseif( HAVE_UTIL_H )
+  set( USE_OPENPTY_H util.h )
+elseif( HAVE_LIBUTIL_H )
+  set( USE_OPENPTY_H libutil.h )
+endif( )
+if( USE_OPENPTY_H )
+  check_c_source_runs("
+      #include <${USE_OPENPTY_H}>
+      int main(int argc, char* argv[]) {
+        int master_fd, slave_fd;
+        int result;
+        result = openpty(&master_fd, &slave_fd, 0, 0, 0);
+        return 0;
+    }"
+    HAVE_OPENPTY
+  )
+endif( )
+if( HAVE_OPENPTY )
+  set( LIB_UTIL util )
+endif( )
 
 tde_restore( CMAKE_REQUIRED_LIBRARIES )
